4f0a546a1f0e1145c23a18043ed8e9990895c694
Ana Flávia Corrêa <anaflaviacorrea@hotmail.com>
2023-05-31 21:31:44 -0300

    first commit clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Clientes
 
-# Create your views here.
+def cadastrar_cliente(request):
+    if request.method == "POST":
+        nome = request.POST.get("Nome")
+        cpf = request.POST.get("CPF")
+        telefone = request.POST.get("Telefone")
+        email = request.POST.get("Email")
+        data_nascimento = request.POST.get("Data_nascimento")
+        endereco = request.POST.get("Endereço")
+
+
+        novo_cliente = Clientes(
+            nome=nome,
+            cpf=cpf,
+            telefone=telefone,
+        )
