be656317ffc19c978151e673fd566d0774c26d26
pdropaullo <pedroabreu11@hotmail.com>
2023-06-01 20:37:33 -0300

    primeiras telas

--- comandas/models.py
+++ comandas/models.py
@@ -1,3 +1,14 @@
+from distutils.command.upload import upload
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Comandas(models.Model):
+    saldo = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.saldo
+
+    class Meta:
+        verbose_name_plural = 'Comandas'
--- comandas/views.py
+++ comandas/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Comandas
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    comandas = Comandas.objects.all()
+    return render(request, 'pages/index.html', {'comandas': comandas})
+
+def recarregar_comanda(request):
+    comandas = Comandas.objects.all()
+    return render(request, 'pages/recarregar_comanda.html', {'comandas': comandas})
+
+def realizar_consumo(request):
+    comandas = Comandas.objects.all()
+    return render(request, 'pages/realizar_consumo.html', {'comandas': comandas})
+
+def ferramentas(request):
+    comandas = Comandas.objects.all()
+    return render(request, 'pages/ferramentas.html', {'comandas': comandas})
+
+def sobre(request):
+    comandas = Comandas.objects.all()
+    return render(request, 'pages/sobre.html', {'comandas': comandas})
